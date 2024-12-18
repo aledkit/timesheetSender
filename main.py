d5ca197981ca2ca2ba936662f3910ce86e2c011b
Alex Kitchen <alexkitchen613@gmail.com>
2024-12-18 03:03:20 -0500

    Fixed error in DEBUG functionality

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from email.mime.text import MIMEText
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 465
 DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-DEBUG = True
+DEBUG = False
 
 def generate_hours():
     """
@@ -126,15 +126,14 @@ def main():
     password = secrets['sender_password']
     receiver_email = secrets['recipient_email']
     test_email = secrets['test_email']
+    if DEBUG:
+        receiver_email = test_email
 
     context = ssl.create_default_context()
 
     message = MIMEMultipart()
     message['From'] = sender_email
-    if DEBUG:
-        message['To'] = test_email
-    else:
-        message['To'] = receiver_email
+    message['To'] = receiver_email
     message['Subject'] = 'Weekly Timesheet'
 
     body = generate_body()
